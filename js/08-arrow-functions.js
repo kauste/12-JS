ce8f0c931162cf7572ce05ac1a7ebeef140726b1
Rugile <rugilestasionyte@gmail.com>
2022-04-04 11:10:44 +0300

    pirma raide

--- js/08-arrow-functions.js
+++ js/08-arrow-functions.js
@@ -24,10 +24,10 @@ console.log(r4)
 
 // kintamajam skirta anonimine funkcija
 
-const atimtis = function (a, b)  {
+const atimtele = function (a, b)  {
     return a - b
 }
-const r5 = atimtis(88,22)
+const r5 = atimtele(88,22)
 console.log(r5)
 
 console.log(`---------`)
@@ -35,18 +35,28 @@ console.log(`---------`)
 const dalyba = (a, b) => {
     return a / b
 }
-const r6 = atimtis(88,22)
+const r6 = dalyba(88,22)
 console.log(r6)
 
 // jei logikos bloke {} yra tik  1 procedura, galima nerasyti {} ir return
 const daugyba = (a, b) =>  a * b
-const r7 = atimtis(88,22)
+const r7 = daugyba(88,22)
 console.log(r7)
 
-// kvadratu. jei parametru bloke yra tik 1 parametras, galime nerasyti ()
+// kvadratu. jei parametru bloke yra tik 1 parametras, galime nerasyti (). bet jei nera nei vieno parametro reikia 0
 
 const kvadratu = a =>  a * a
 const r8 = kvadratu(pirmas)
 console.log(r8)
 
+const tekstas = 'Labas rytas' 
+function pirmaRaide(text) {
+    return text[0];
+}
+console.log(pirmaRaide(tekstas));
 
+const tekstukas =  'Labas rytas' 
+function pirmaRaide(text) {
+    return text[0];
+}
+console.log(pirmaRaide(tekstas));
