9966a131ed45e7d14951fe5ab3ec5bca184fd218
Rugile <rugilestasionyte@gmail.com>
2022-04-11 05:52:33 +0300

    class update

--- js/16-object.js
+++ js/16-object.js
@@ -3,51 +3,55 @@ OBJECT
 */
 
 /*, 
-Mokiniai:
-    Mokinys: 
+Mokiniai (array):
+    Mokinys (??): 
     - varda (string)
     - pazymius, array
-    - kontaktini asmeni ()
+    - kontaktini asmeni (??)
         - varda  (string)
         - tel nr number
 */
 const students = [
-  {  // { }- reprezentuoja objekta
+   {  // { }- reprezentuoja objekta
       // key: value, - key- kokia informacija turi sedeti, value- konkreti reiksme
-      name: 'Petras',
-      marks: [10, 6,],
-      contacts: {
-          name: 'P.P',
-          phone : 111,
-          adress : {
-              city: 'Vilnius',
-              street: 'Azuolo'
-          },
-        },
-  },
-  {
-      name: 'Jonas',
-      marks: [10, 9],
-      contacts: {
-          name: 'J.J',
-          phone : 222,
-          adress : {
-            city: 'Vilnius',
-            street: 'Azuolo'
-        },
+        name: 'Petras',
+        marks: [
+            10, 
+            6,
+        ],
+        contacts: {
+            name: 'P.P',
+            phone : 111,
+            adress : {
+                city: 'Vilnius',
+                street: 'Azuolo'
+            },
         },
     },
-]
+   {
+        name: 'Jonas',
+        marks: [10, 9],
+        contacts: {
+            name: 'J.J',
+            phone : 222,
+            adress : {
+                city: 'Vilnius',
+                street: 'Lubinu'
+            },
+        },
+    },
+];
 
 
-const studentIndex = 1;
-const student = students[studentIndex];
-const name = student.name; //['name'];      /*masyve raktazodziai automatizuoti(0, 1, 2), o objekte- sukuriame*/
+const studentIndex = 0;
+const student = students[studentIndex]; //masyvuose tuo tarpu studets.2 neprarasysi
+const name = student.name; //=== name = student['name'];      /*masyve raktazodziai automatizuoti(0, 1, 2), o objekte- sukuriame*/
 const contact = student.contacts;//student['contacts']
 const contactName = contact.name; // contact['name']*/
 const contactPhome = student.contacts.phone;
 const contacCity = student.contacts.adress.city;
 
+console.log(student);
 console.log(name);
 console.log(contactName);
  ///
--- js/17-class.js
+++ js/17-class.js
@@ -11,52 +11,113 @@ console.log(rez)
 
 /*
 CLASS: savybiu ir metodu junginys/grupe.
-Savybes- kintamieji (info); pvz koks radijos daznis ijungtas
-Metodai -funkcionalumas; dazni didinti/mazinti
+Savybes- kintamieji (objektyvi informacija aprie objekta); pvz kokia radijos busena: ijungta ar isjungta, koks garas, koks daznis ijungtas (kokia stotis), kokiam bangu diapazone yra daznis;       pvz, kokia masinos spalva, kiek ratu, kiek pedalu, kokia pavara....
+Metodai -funkcionalumas. FUNKCIJOS JAU TURI KONTEKSTA- SKAICIJUOJA JAU BUTENT OBJEKTUI, KITUR PANAUDOTI NEGALI.; ijunk/isjunk radija, padidink garsa/sumazink garsa, dazni didinti/mazinti (perjungt stoti);                                                                    suskaiciuoti, jei automobilio bake tiek kuro, kiek jis nuvaziuos, po kiek laiko vel uzpilti baka...
 
 klases neturi skiaustu
-klases- savotiska receptura
+klases- savotiska receptura, bet ne veikiantis dalykas
 Objektas- pagal receptura pagamintas kokretus/ apciuopiamas dalykas
 */
 
-class Bankomatas {
+class Bankomatas { // KLASES IS DIDZIOSIOS, SUSITARIMO REIKALAS
     constructor(street, color) {
-        this.gatve = street;
+        this.gatve = street; // GALI BUTI IR THIS.STREET = STREET;
         this.spalva = color;
         this.turnedOn = false;
          this.money = 0;
     }
     switchOn () {
         this.turnedOn = true;
+        console.log(`${this.gatve} bankomatas ijungtas`);
     }
     switchOff () {
-        this.turnedOff = false;
+        this.turnedOn = false;
+        console.log(`${this.gatve} bankomatas isjungtas`);
     }   
-    cashIn (){
-        if(this.turnedOn){
-            console.log('Bandoma inesti pinikgus')
-        } else {
-            console.log('Panasu, jog nera elektros')
+    cashIn (money) { // molney parametras tik sito metodo viduje, riestiniai gniauztai jo apimtis
+        //jei neijungtas-   ERROR
+        //jei ne  normalus skaicius- ERROR
+        //ISKAICIUOJAM
+        if(!this.turnedOn){
+            console.log(`Panasu, kad ${this.gatve} bankomate nera elektros`);
+            return;
         }
+        if(typeof money !=='number' 
+        || !isFinite(money)) {
+            console.log(this.gatve, 'Nepanasu i tikra piniga');
+            return;
+        }
+        if (money <= 0) {
+            console.log(this.gatve, 'Minusiniu pinigu nebuna');
+            return;
+        }
+
+        this.money += money; // this.money -globalus parametras; money -lokalus
+        console.log(`${this.gatve}: inesta ${money} pinigu`);
     }
-    cashOut (){
-        console.log('Bandoma isgryninti pinigus')
+    cashOut (money){
+        if(!this.turnedOn){
+            console.log(this.gatve, 'Panasu, kad nera elektros');
+            return;
+        }
+        if(typeof money !=='number' 
+        || !isFinite(money)) {
+            console.log(this.gatve, 'Nepanasu i tikra piniga');
+            return;
+        }
+        if (money <= 0) {
+            console.log(this.gatve, 'Minusiniu pinigu nebuna');
+            return;
+        }
+        if (this.money < money) {
+            console.log(this.gatve, 'Bankomate siuo metu nera tokios sumos');
+            return;
+        }
+        this.money -= money;
+        console.log(`${this.gatve}: isgryninta ${money} pinigu`)
+    }
+    status(){
+        console.log( `${this.gatve} siuo metu pinigu likutis yra ${this.money}`)
     }
 }
-
-const bankomatas1 = new Bankomatas('Didzioji g. 2', 'red');
+console.log('---------------------------')
+const bankomatas1 = new Bankomatas('Didzioji g. 2', 'red'); // JAU REALUS BANKOMARAS
 console.log(bankomatas1);
+
+console.log('---------------------------')
 const bankomatas2 = new Bankomatas('Mazoji g. 3', 'blue');
 console.log(bankomatas2.gatve);
 
 
 //ijunkime
-
+console.log('---------------------------')
 bankomatas1.turnedOn = true; // nelabai geras budas
-console.log(bankomatas1);
 
+console.log('---------swich On------------------')
 bankomatas2.switchOn()
-bankomatas2.switchOff()
-console.log(bankomatas2);
 
-bankomatas1.cashIn()
+//isjunkime
+console.log('-----------swich of----------------')
+bankomatas1.switchOff();
+bankomatas2.switchOff();
+
+
+console.log('----ineskim pinigus---------');
+
+bankomatas1.cashIn();
+
+console.log('---ijunkime ineskim pinigus-----');
+bankomatas1.switchOn();
+bankomatas2.switchOn();
+bankomatas1.cashIn(true);
+bankomatas1.cashIn(-100);
+bankomatas1.cashIn(100);
+bankomatas1.cashIn(5);
+bankomatas2.cashIn(5);
+bankomatas1.cashIn(12);
+console.log('--- iseskim pinigus-----');
+bankomatas1.cashOut(15);
+bankomatas1.cashOut(150);
+
+bankomatas1.status()
+bankomatas2.status()
